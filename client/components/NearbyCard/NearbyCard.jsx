d8b4357bab2bb04961ca14f05791d08bcbff6645
jared <jcraig1730@gmail.com>
2019-08-30 13:41:06 -0700

    circleci test commit

--- client/components/NearbyCard/NearbyCard.jsx
+++ client/components/NearbyCard/NearbyCard.jsx
@@ -7,7 +7,7 @@ class NearbyCard extends React.Component {
     super(props);
 
   }
-  // arbitrary change
+  // arbitrary change1
   render() {
     const { placeDetails } = this.props;
     const avgRating = placeDetails.ratings.reduce((review, accum) => accum + review) / placeDetails.ratings.length;
